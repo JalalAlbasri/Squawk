042c4e51606746637d3a874dd358b72abd152218
Jalal Albasri <jalal.albasri@gmail.com>
2013-08-05 20:27:00 +1000

    Fixed schema definition for Google App Engine, Added twitter-queue, Implemented TwitterServlet, DeviceInfoEnpoint, DeviceInfo, Added Tweet Class,
    Methods untested..

--- Squawk-AppEngine/src/main/java/com/jalbasri/squawk/TwitterServlet.java
+++ Squawk-AppEngine/src/main/java/com/jalbasri/squawk/TwitterServlet.java
@@ -13,7 +13,6 @@ import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-
 import javax.persistence.EntityManager;
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
